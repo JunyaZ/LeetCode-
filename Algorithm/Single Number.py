b0db46f52b311bfc84f00c461783c605a8929679
Junya Zhao <31264540+JunyaZ@users.noreply.github.com>
2020-04-15 15:29:33 -0400

    Create Single Number.py

---
+++ Algorithm/Single Number.py
@@ -0,0 +1,37 @@
+"""
+Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+
+Note:
+
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+Example 1:
+
+Input: [2,2,1]
+Output: 1
+Example 2:
+
+Input: [4,1,2,1,2]
+Output: 4
+"""
+
+from collections import defaultdict
+class Solution:
+    def singleNumber_v1(self, nums: List[int]) -> int:
+        hash_table = defaultdict(int)
+        for i in nums:
+            hash_table[i] += 1
+        
+        for i in hash_table:
+            if hash_table[i] == 1:
+                return i
+
+    def singleNumber_v2(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        a = 0
+        for i in nums:
+            a ^= i
+        return a
