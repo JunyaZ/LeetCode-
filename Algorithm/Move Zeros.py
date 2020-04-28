ec6610f4721998d9ffa8bee92f5a59ee01cd0939
Junya Zhao <31264540+JunyaZ@users.noreply.github.com>
2020-04-27 23:22:55 -0400

    Update Move Zeros.py

--- Algorithm/Move Zeros.py
+++ Algorithm/Move Zeros.py
@@ -11,3 +11,13 @@ Note:
 You must do this in-place without making a copy of the array.
 Minimize the total number of operations.
 """
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in nums:
+            if i == 0:
+                nums.remove(i)
+                nums.append(i)
+        return nums
