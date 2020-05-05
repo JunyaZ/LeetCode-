cad7b437695b1d6fa339033c34b59ac4fa52421f
Junya Zhao <31264540+JunyaZ@users.noreply.github.com>
2020-05-05 15:29:39 -0400

    Update First Unique Character in a String.py

--- Algorithm/First Unique Character in a String.py
+++ Algorithm/First Unique Character in a String.py
@@ -10,3 +10,14 @@ s = "loveleetcode",
 return 2.
 Note: You may assume the string contain only lowercase letters.
 """
+from collections import defaultdict
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        nums=defaultdict(int)
+        for i in s:
+            nums[i]+=1   
+        for i in nums:
+            if nums[i]==1:
+                return s.index(i)
+        else:
+            return -1
