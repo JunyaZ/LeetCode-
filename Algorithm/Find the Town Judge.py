31320602a48b6ed51faefd613df763da3a7708cb
Junya Zhao <31264540+JunyaZ@users.noreply.github.com>
2020-05-10 21:12:44 -0400

    Update Find the Town Judge.py

--- Algorithm/Find the Town Judge.py
+++ Algorithm/Find the Town Judge.py
@@ -1,1 +1,19 @@
 
+from collections import defaultdict
+class Solution:
+    def findJudge(self, N: int, trust: List[List[int]]) -> int:
+        if N==1:
+            return 1
+        else:
+            score=defaultdict(int)
+            for l1,l2 in trust:
+                score[l1]-=1
+                score[l2]+=1
+            for j in score:
+                if score[j]==N-1:
+                    return j
+            else:
+                return -1
+                
+            
+            
