6c712b153a2a49673b03ba78da1568ea18ff6d63
Junya Zhao <31264540+JunyaZ@users.noreply.github.com>
2020-05-12 20:14:35 -0400

    Update Single Number.py

--- Algorithm/Single Number.py
+++ Algorithm/Single Number.py
@@ -1,4 +1,5 @@
 """
+Single Number:
 Given a non-empty array of integers, every element appears twice except for one. Find that single one.
 
 Note:
@@ -35,3 +36,23 @@ class Solution:
         for i in nums:
             a ^= i
         return a
+    
+"""
+Single Element in a Sorted Array:
+
+You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears exactly once. Find this single element that appears only once.
+
+ 
+
+Example 1:
+
+Input: [1,1,2,3,3,4,4,8,8]
+Output: 2
+Example 2:
+
+Input: [3,3,7,7,10,11,11]
+Output: 10
+ 
+
+Note: Your solution should run in O(log n) time and O(1) space
+"""
